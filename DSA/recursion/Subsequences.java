8211c2cf31d7a5233101ad3dc3984e5a0437b344
Siddhi Patil <sonupatil4330@gmail.com>
2026-01-12 21:46:24 +0530

    Done some Advanced recursion problems using HashSet

---
+++ DSA/recursion/Subsequences.java
@@ -0,0 +1,18 @@
+public class Subsequences {
+    public static void subsequences(String str, int idx, String newStr) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        // to be
+        subsequences(str, idx + 1, newStr + currChar);
+        // not to be
+        subsequences(str, idx + 1, newStr);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequences(str, 0, "");
+    }
+}
